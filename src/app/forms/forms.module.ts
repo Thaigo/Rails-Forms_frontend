963f41c597709d8243532fd48d798c0cd2a6d550
Thaigo_Freitas <tmfreitas@outlook.com>
2018-03-06 22:15:18 -0300

    autenticando com a API

--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormListComponent } from './form-list/form-list.component';
 import { ActiveComponent } from './shared/active/active.component';
+import { FormEditComponent } from './form-edit/form-edit.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [FormListComponent, ActiveComponent]
+  declarations: [FormListComponent, ActiveComponent, FormEditComponent]
 })
 export class FormsModule { }
