f8dec6c2bbda15988175c0c86a38fd20b95bf5bc
Thaigo_Freitas <tmfreitas@outlook.com>
2018-03-12 23:29:36 -0300

    component formList

--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormListComponent } from './form-list/form-list.component';
-import { ActiveComponent } from './shared/active/active.component';
 import { FormEditComponent } from './form-edit/form-edit.component';
+import { ActiveComponent } from './shared/active/active.component';
+import { MaterializeModule } from 'ng2-materialize';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { routing } from './../app.routing';
+import { FormsModule as CoreFormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserAnimationsModule,
+    MaterializeModule.forRoot(),
+    routing,
+    CoreFormsModule
   ],
-  declarations: [FormListComponent, ActiveComponent, FormEditComponent]
+  exports: [
+    FormListComponent
+  ],
+  declarations: [FormListComponent, FormEditComponent, ActiveComponent]
 })
 export class FormsModule { }
